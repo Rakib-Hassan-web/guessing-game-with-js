26552818e836b14b15dedb696545cb684633e026
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-01 20:29:14 +0600

    dom add

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+// -----------dom ------------------
+
+
+// -------------player 1------------
+let main1 =document.querySelector('.main1')
+let error1 =document.querySelector('.error1')
+let input1 =document.querySelector('.input1')
+let button1 =document.querySelector('.button1')
+
+
+
+// ------------player 2 dom---------
+
+let main2 =document.querySelector('.main2')
+let chance =document.querySelector('.chance')
+let input2 =document.querySelector('.input2')
+let error2 =document.querySelector('.error2')
+let button2 =document.querySelector('.button2')
+
+
+// ----Winner dom--------
+
+let winner =document.querySelector('.winner')
+let winnerimg =document.querySelector('.winnerimg')
+let winer_name =document.querySelector('.winer_name')
+
+
+
+
+
+
+
+
+
